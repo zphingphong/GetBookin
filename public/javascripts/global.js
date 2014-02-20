783af93f39e0c8b1027177e0c36991fd44f84e33
ZoM <pkhantidhara@zinzone.net>
2014-02-20 15:59:59 -0800

    Using Facebook SDK with jQuery.

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -20,22 +20,17 @@ window.getBookinNgApp.run(function($rootScope) {
 //}]);
 
 /******************************** [START] Facebook setup ***********************************/
-window.fbAsyncInit = function() {
-    FB.init({
-        appId      : '640875779319763',
-        status     : true, // check login status
-        cookie     : true, // enable cookies to allow the server to access the session
-        xfbml      : true  // parse XFBML
+$(document).ready(function() {
+    $.ajaxSetup({ cache: true });
+    $.getScript('//connect.facebook.net/en_US/all.js', function(){
+        FB.init({
+            appId       : '640875779319763',
+            status      : true, // check login status
+            cookie      : true, // enable cookies to allow the server to access the session
+            xfbml       : true  // parse XFBML
+        });
     });
-};
-
-(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/all.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+});
 /******************************** [END] Facebook setup ***********************************/
 
 
