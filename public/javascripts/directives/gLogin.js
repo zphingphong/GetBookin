d3aa230e9301b9657bd5e638a95672c3b93d9c18
ZoM <pkhantidhara@zinzone.net>
2014-03-02 21:57:16 -0800

    Get Google user information

--- public/javascripts/directives/gLogin.js
+++ public/javascripts/directives/gLogin.js
@@ -12,7 +12,36 @@ window.getBookinNgApp.directive('glogin', function(){
             $scope.gLogin = function(){
                 gapi.auth.signIn({
                     'callback': function(authResult){
-                        console.log(authResult);
+                        if (authResult.status.signed_in == 'true') {
+                            gapi.client.load('plus','v1', function(){
+                                var request = gapi.client.plus.people.get({
+                                    'userId': 'me'
+                                });
+                                request.execute(function(response) {
+                                    console.log(response);
+//                                    $http.post('/user', {
+//                                        name: response.name,
+//                                        email: response.email,
+//                                        facebookId: response.id,
+//                                        accountType: 'user'
+//                                    }).success(function(response) {
+//                                        if(response.success){
+//                                            console.log($cookies.user);
+//                                            if(response.user.accountType == 'user'){
+//                                                $rootScope.$emit('userLoggedIn', {
+//                                                    user: response.user
+//                                                });
+//                                            } else if(response.user.accountType == 'admin'){
+//                                                $window.location.href = '/pages/adminSchedule';
+//                                            }
+//                                        }
+//                                    });
+                                });
+                            });
+                        } else {
+                            //user hit cancel button
+                            console.log('User cancelled login or did not fully authorize.');
+                        }
                     }
                 });
             }
