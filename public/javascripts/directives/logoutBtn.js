0fcbe12d4ba08f888cb475145d06f53af3ca3d6f
ZoM <pkhantidhara@zinzone.net>
2014-03-03 02:19:23 -0800

    Add logout button to admin

--- public/javascripts/directives/logoutBtn.js
+++ public/javascripts/directives/logoutBtn.js
@@ -5,7 +5,7 @@
 window.getBookinNgApp.directive('logoutbtn', function(){
     return {
         restrict: 'AE',
-        template: '<button class="btn btn-default navbar-btn" ng-click="logout()"><i class="fa fa-power-off"></i>Sign Out</button>',
+        template: '<button class="btn btn-default navbar-btn" ng-click="logout()"><i class="fa fa-power-off"></i> Sign Out</button>',
         link: function(scope, element, attrs){
         },
         controller: function($rootScope, $scope, $cookies, $window){
