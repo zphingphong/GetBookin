4ce0df27f42f12043147a2ad3aa266bc758fcc54
ZoM <pkhantidhara@zinzone.net>
2014-03-03 01:56:55 -0800

    Added logout button directive.

---
+++ public/javascripts/directives/logoutBtn.js
@@ -0,0 +1,17 @@
+/**
+ * Created by ZoM on 20/03/03.
+ */
+
+window.getBookinNgApp.directive('logoutbtn', function(){
+    return {
+        restrict: 'AE',
+        template: '<button class="btn btn-default navbar-btn" ng-click="logout()"><i class="fa fa-power-off"></i>Sign Out</button>',
+        link: function(scope, element, attrs){
+        },
+        controller: function($rootScope, $scope, $cookies, $window){
+            $scope.logout = function(){
+            }
+        }
+    }
+});
+
